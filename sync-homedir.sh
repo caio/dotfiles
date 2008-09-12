2c318b4474edf7371e335ea0eb46578871a2962b
Caio Romão <caio@linux.vnet.ibm.com>
2008-09-12 16:43:42 -0300

    Added new files to sync-homedir.sh

--- sync-homedir.sh
+++ sync-homedir.sh
@@ -12,9 +12,9 @@ RM=/bin/rm
 CP=/bin/cp
 
 # Removing files
-${RM} -rf ~/.vim* ~/.bashrc ~/.hgrc ~/.mplayer ~/.source ~/.Xdefaults ~/.pythonrc.py ~/.gitconfig
+${RM} -rf ~/.vim* ~/.bashrc ~/.hgrc ~/.mplayer ~/.source ~/.Xdefaults ~/.pythonrc.py ~/.gitconfig ~/.irssi ~/.screenrc
 
 # Copying new ones
-${CP} -R .vim* .bashrc .mplayer .source .Xdefaults .pythonrc.py .gitconfig ~
+${CP} -R .vim* .bashrc .mplayer .source .Xdefaults .pythonrc.py .gitconfig .irssi .screenrc ~
 ${CP} hgrc ~/.hgrc
 
