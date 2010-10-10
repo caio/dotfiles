03abe15cdf2c23c99da81a75b9c0517f8e5e54af
Caio Romão <caioromao@gmail.com>
2010-10-09 21:32:39 -0300

    implement make_dir_complete.

--- source/projects_complete.sh
+++ source/projects_complete.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
-# http://sixohthree.com/867/bash-completion
 
-jk() { cd ~/src/$1; }
-__proj() {
-    local cur len wrkdir
-    wrkdir=$(readlink -f ~/src)
-    cur=${COMP_WORDS[COMP_CWORD]}
-    len=$((${#wrkdir} + 2))
-    COMPREPLY=( $(compgen -S/ -d $wrkdir/$cur| cut -b $len-) )
+make_dir_complete() {
+    local aliasname=$1
+    local dirname=$(readlink -f $2)
+    local prgname="__s_${aliasname}__"
+    FUNC="function $prgname() {
+        local cur len wrkdir;
+        wrkdir=\"$dirname\"
+        cur=\${COMP_WORDS[COMP_CWORD]};
+        len=\$((\${#wrkdir} + 2));
+        COMPREPLY=( \$(compgen -S/ -d \$wrkdir/\$cur| cut -b \$len-) );
+    }"
+    ALIAS="$aliasname () { cd $dirname/\$1; }"
+    eval $FUNC
+    eval $ALIAS
+    complete -o nospace -F $prgname $aliasname
 }
-complete -o nospace -F __proj jk
 
+make_dir_complete dl ~/download/
+make_dir_complete jk ~/src/
+make_dir_complete h ~/
