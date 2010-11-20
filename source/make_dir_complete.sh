c71c344f7032595dc945e0854e9ef4b332a0e17a
Caio Romão <caioromao@gmail.com>
2010-11-20 11:48:04 -0200

    Properly complete directories with spaces

--- source/make_dir_complete.sh
+++ source/make_dir_complete.sh
@@ -6,12 +6,13 @@ make_dir_complete() {
     local prgname="__s_${aliasname}__"
     FUNC="function $prgname() {
         local cur len wrkdir;
+        local IFS=\$'\\n'
         wrkdir=\"$dirname\"
         cur=\${COMP_WORDS[COMP_CWORD]};
         len=\$((\${#wrkdir} + 2));
         COMPREPLY=( \$(compgen -S/ -d \$wrkdir/\$cur| cut -b \$len-) );
     }"
-    ALIAS="$aliasname () { cd $dirname/\$1; }"
+    ALIAS="$aliasname () { cd \"$dirname/\$*\"; }"
     eval $FUNC
     eval $ALIAS
     complete -o nospace -F $prgname $aliasname
