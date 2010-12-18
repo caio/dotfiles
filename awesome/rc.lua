3ac10dd9fb1c03efaf3822148b410c666f4c4632
Caio Romão <caioromao@gmail.com>
2010-12-17 23:45:24 -0200

    Who cares about nano anyway?

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -20,7 +20,7 @@ beautiful.init("/home/romao/.config/awesome/themes/zenburn/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "urxvt"
-editor = os.getenv("EDITOR") or "nano"
+editor = os.getenv("EDITOR") or "vim"
 editor_cmd = terminal .. " -e " .. editor
 
 -- Default modkey.
