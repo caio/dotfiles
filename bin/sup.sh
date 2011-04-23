e4b16d39f2e8c127aa1d5f3d04d9be3ae777e38b
Caio Romão <caioromao@gmail.com>
2011-04-23 20:50:54 -0300

    Use older 1.9.2 release for sup

--- bin/sup.sh
+++ bin/sup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-tmux new-session -d -s mail -n MAIL ". ~/.rvm/scripts/rvm; rvm use 1.9.2; sup"
+tmux new-session -d -s mail -n MAIL ". ~/.rvm/scripts/rvm; rvm use ruby-1.9.2-p136; sup"
 tmux split-window -p 7 -t mail:0 'offlineimap'
 tmux select-pane -U -t mail:0
 tmux attach-session -d -t mail
