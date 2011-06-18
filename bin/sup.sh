1b0f286f3706236cd76f83114a3331ac6da9d5f8
Caio Romão <caioromao@gmail.com>
2011-06-18 16:17:09 -0300

    Remove sup.sh as I'm not using sup anymore

--- bin/sup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-tmux new-session -d -s mail -n MAIL ". ~/.rvm/scripts/rvm; rvm use ruby-1.9.2-p136; sup"
-tmux split-window -p 7 -t mail:0 'offlineimap'
-tmux select-pane -U -t mail:0
-tmux attach-session -d -t mail
