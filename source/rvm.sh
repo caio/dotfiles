47de2398bfb107c009c6c237e4a1e88dd9c0eb38
Caio Romão <caioromao@gmail.com>
2011-11-17 17:03:08 -0200

    Fix variable name conflict on __load_rvm

--- source/rvm.sh
+++ source/rvm.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 __load_rvm() {
-    local rvm_path="${HOME}/.rvm"
+    local rvm_load_path="${HOME}/.rvm"
     test ! -d ${rvm_path} && return 0
-    . ${rvm_path}/scripts/rvm
-    . ${rvm_path}/scripts/completion
+    . ${rvm_load_path}/scripts/rvm
+    . ${rvm_load_path}/scripts/completion
 }
 
 __load_rvm
