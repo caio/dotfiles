844a157fcde3ccb5d9ddd26c4cc2312536c98c9d
Caio Romão <caioromao@gmail.com>
2012-03-14 09:58:57 -0300

    Fix rvm loading code

--- source/rvm.sh
+++ source/rvm.sh
@@ -2,7 +2,7 @@
 
 __load_rvm() {
     local rvm_load_path="${HOME}/.rvm"
-    test ! -d ${rvm_path} && return 0
+    test ! -d ${rvm_load_path} && return 0
     . ${rvm_load_path}/scripts/rvm
     . ${rvm_load_path}/scripts/completion
 }
