a9a47810bb0ecf9d6e6d1a157e0883e6d0c0aeab
Caio Romão <caioromao@gmail.com>
2012-10-21 15:47:29 +0200

    Use spawn_with_shell on awesome scratchpad

--- awesome/scratch/drop.lua
+++ awesome/scratch/drop.lua
@@ -97,7 +97,7 @@ function toggle(prog, vert, horiz, width, height, sticky, screen)
 
         -- Add manage signal and spawn the program
         capi.client.add_signal("manage", spawnw)
-        awful.util.spawn(prog, false)
+        awful.util.spawn_with_shell(prog, false)
     else
         -- Get a running client
         c = dropdown[prog][screen]
