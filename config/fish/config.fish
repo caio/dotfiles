00aaf1abfecc47702919f54d8c5e103d2d0a0bd5
Caio Romão <hello@caio.co>
2019-02-04 11:34:05 +0100

    Add .poetry/bin to PATH

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -5,7 +5,7 @@ set -g fish_color_error red --bold
 
 fish_vi_key_bindings
 
-set -U fish_user_paths {$HOME}/bin {$GOPATH}/bin {$HOME}/.cargo/bin
+set -U fish_user_paths {$HOME}/bin {$GOPATH}/bin {$HOME}/.cargo/bin {$HOME}/.poetry/bin
 
 abbr -a -- - 'cd -'
 
