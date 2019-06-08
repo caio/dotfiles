e925c7416f09f4397c13c2e3012e32734eb65ee0
Caio Romão <hello@caio.co>
2019-06-08 21:15:59 +0200

    Cosmetic change to fish_user_paths setup

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -5,7 +5,11 @@ set -g fish_color_error red --bold
 
 fish_vi_key_bindings
 
-set -U fish_user_paths {$HOME}/bin {$GOPATH}/bin {$HOME}/.cargo/bin {$HOME}/.poetry/bin
+set -U fish_user_paths \
+    {$GOPATH}/bin \
+    {$HOME}/.cargo/bin \
+    {$HOME}/.poetry/bin \
+    {$HOME}/bin
 
 abbr -a -- - 'cd -'
 
