1c8105e7aa39113ecf1485403329c509620cf78d
Caio Romão <hello@caio.co>
2019-08-14 21:19:16 +0200

    Less fancyness in the prompt

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -28,13 +28,13 @@ function fish_prompt --description 'Write out the prompt'
         set -g __fish_git_prompt_char_stagedstate "●"
     end
     if not set -q __fish_git_prompt_char_dirtystate
-        set -g __fish_git_prompt_char_dirtystate "✚"
+        set -g __fish_git_prompt_char_dirtystate "+"
     end
     if not set -q __fish_git_prompt_char_untrackedfiles
         set -g __fish_git_prompt_char_untrackedfiles "…"
     end
     if not set -q __fish_git_prompt_char_conflictedstate
-        set -g __fish_git_prompt_char_conflictedstate "✖"
+        set -g __fish_git_prompt_char_conflictedstate "x"
     end
     if not set -q __fish_git_prompt_char_cleanstate
         set -g __fish_git_prompt_char_cleanstate "✔"
