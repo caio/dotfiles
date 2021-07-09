e4b3179df5ea637707cca1872cb11e3a5f77c86c
Caio Romão <hello@caio.co>
2021-07-09 12:37:13 +0200

    Use nord as the default color scheme

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -2,6 +2,12 @@ return require('packer').startup(function()
     use 'wbthomason/packer.nvim'
 
     use {
+        'arcticicestudio/nord-vim',
+        -- Does it need to be this verbose?
+        config = [[vim.api.nvim_command "colorscheme nord"]]
+    }
+
+    use {
         'nvim-treesitter/nvim-treesitter',
         run = ':TSUpdate',
         config = [[require('config.treesitter')]]
