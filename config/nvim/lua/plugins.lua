1ccfbccfbb7c3dcd90bd549c680aecfcd58cb1cb
Caio Romão <hello@caio.co>
2021-07-10 13:43:04 +0200

    Use a function instead of string-eval

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -3,10 +3,10 @@ return require('packer').startup(function()
 
     use {
         'lukas-reineke/indent-blankline.nvim',
-        config = [[
+        config = function()
             vim.g.indent_blankline_show_first_indent_level = false
             vim.g.indent_blankline_show_trailing_blankline_indent = false
-        ]]
+        end
     }
 
     use {
