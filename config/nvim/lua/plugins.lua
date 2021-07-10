3e7bcb46080e5d47978ff2c6ec0a725934dec45d
Caio Romão <hello@caio.co>
2021-07-10 13:25:12 +0200

    Show indent guides

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -2,6 +2,14 @@ return require('packer').startup(function()
     use 'wbthomason/packer.nvim'
 
     use {
+        'lukas-reineke/indent-blankline.nvim',
+        config = [[
+            vim.g.indent_blankline_show_first_indent_level = false
+            vim.g.indent_blankline_show_trailing_blankline_indent = false
+        ]]
+    }
+
+    use {
         'arcticicestudio/nord-vim',
         -- Does it need to be this verbose?
         config = [[vim.api.nvim_command "colorscheme nord"]]
