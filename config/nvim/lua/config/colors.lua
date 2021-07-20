7cd5251e7af4198127ec9b46eccf399522f89bfe
Caio Romão <hello@caio.co>
2021-07-20 13:15:43 +0200

    ToggleStyle to change between palenight and lighter styles

--- config/nvim/lua/config/colors.lua
+++ config/nvim/lua/config/colors.lua
@@ -5,6 +5,19 @@ vim.g.material_italic_comments = true
 
 vim.cmd("colorscheme material")
 
+function toggle_style()
+    local change_style = require('material.functions').change_style
+
+    if vim.g.material_style == 'palenight' then
+        change_style('lighter')
+    else
+        change_style('palenight')
+    end
+end
+
+vim.cmd([[command! ToggleStyle lua toggle_style()]])
+
+
 require('lualine').setup({
     options = {
         theme = 'dracula',
