dbdc23febf73f6b961aa1ffe17be2e6de9ef6346
Caio Romão <hello@caio.co>
2021-07-26 12:23:41 +0200

    Add some comments for the plugin groups

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -1,6 +1,7 @@
 return require('packer').startup(function()
     use 'wbthomason/packer.nvim'
 
+    -- General-purpose editing plugins
     use {
         'tpope/vim-commentary',
         'tpope/vim-repeat',
@@ -8,6 +9,7 @@ return require('packer').startup(function()
         'cohama/lexima.vim',
     }
 
+    -- Visual indentation guides
     use {
         'lukas-reineke/indent-blankline.nvim',
         config = function()
@@ -16,25 +18,31 @@ return require('packer').startup(function()
         end
     }
 
+    -- Theme
     use 'marko-cerovac/material.nvim'
 
+    -- Statusbar
     use {
         'hoob3rt/lualine.nvim',
         config = [[require('config.colors')]]
     }
 
+    -- Fancyful pop-up/floating windows with fuzzy finding support
     use {
         'nvim-telescope/telescope.nvim',
         requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}},
         config = [[require('config.telescope')]]
     }
 
+    -- Settings for the built-in LSP client
+    -- Only gets loaded when `:LspStart` is called manually
     use {
         'neovim/nvim-lspconfig',
         cmd = 'LspStart',
         config = [[require('config.lsp')]]
     }
 
+    -- Treesitter-based syntax support
     use {
         'nvim-treesitter/nvim-treesitter',
         run = ':TSUpdate',
