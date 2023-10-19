2a4daf72dac43f370e2fc346234e558da9d508ef
Caio <contact@caio.co>
2023-10-19 09:45:10 +0200

    you gotta be fucking kidding me

---
+++ config/fish/functions/fish_command_not_found.fish
@@ -0,0 +1,7 @@
+function fish_command_not_found
+    # leave my disk alone, $distros
+    # what's next? sending my cmdline to a remote service
+    # to correct it? then you can upsell some garbage
+    # software?
+    __fish_default_command_not_found_handler $argv
+end
