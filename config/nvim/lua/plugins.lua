57989573080bcaf6ad652625462bd7209b7dd824
Caio <contact@caio.co>
2024-03-19 10:17:14 +0100

    Fix broken autopairs + <enter> combo

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -24,20 +24,14 @@ return require('packer').startup(function()
 
     use {
         'windwp/nvim-autopairs',
+        requires = { 'hrsh7th/nvim-cmp' },
         config = function()
             local autopairs = require('nvim-autopairs')
-
             autopairs.setup({})
 
-            function complete_or_autopairs()
-                if vim.fn.pumvisible() ~= 0  then
-                    return autopairs.esc("<cr>")
-                else
-                    return autopairs.autopairs_cr()
-                end
-            end
-
-            vim.keymap.set('i' , '<CR>','v:lua.complete_or_autopairs()', {expr = true})
+            local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+            local cmp = require('cmp')
+            cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
         end
     }
 
