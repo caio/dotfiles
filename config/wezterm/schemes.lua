bec03468a923b72fc31dc19ad172e7c9c9f71d84
Caio <contact@caio.co>
2024-04-06 08:51:36 +0200

    wezterm: zenwritten for dark/light scheme
    why play with saturation when i can haz zero

--- config/wezterm/schemes.lua
+++ config/wezterm/schemes.lua
@@ -1,67 +1,68 @@
 return {
-  -- zenbones light, with a dimmer bg and no black/white swap
+  -- zenwritten background=light
   ['light'] = {
-    background = "#e8e4e3",
-    foreground = "#2c363c",
-    cursor_bg = "#2c363c",
-    cursor_fg = "#e8e4e3",
-    cursor_border = "#2c363c",
-    selection_bg = "#d2dfe7",
-    selection_fg = "#2c363c",
+    foreground    = "#353535",
+    background    = "#EEEEEE",
+    cursor_fg     = "#EEEEEE",
+    cursor_bg     = "#353535",
+    cursor_border = "#EEEEEE",
+    selection_fg  = "#353535",
+    selection_bg  = "#D7D7D7",
 
     ansi = {
-      "#2c363c",
-      "#a8334c",
-      "#617437",
+      "#EEEEEE",
+      "#A8334C",
+      "#4F6C31",
       "#944927",
       "#286486",
-      "#88507d",
-      "#3b8992",
-      "#f0edec",
+      "#88507D",
+      "#3B8992",
+      "#353535"
     },
 
     brights = {
-      "#44525b",
-      "#9c2842",
-      "#55672a",
-      "#87411e",
-      "#1f5a7a",
-      "#864079",
-      "#2f7c85",
-      "#dcd2ce",
-    }
+      "#C6C3C3",
+      "#94253E",
+      "#3F5A22",
+      "#803D1C",
+      "#1D5573",
+      "#7B3B70",
+      "#2B747C",
+      "#5C5C5C"
+    },
+
   },
 
-  -- zenbones dark, with a warmer background
+  -- zenwritten background=dark
   ['dark'] = {
-    background = "#221f1d",
-    foreground = "#b4bdc3",
-    cursor_bg = "#c4cacf",
-    cursor_fg = "#221f1d",
-    cursor_border = "#c4cacf",
-    selection_bg = "#3d4042",
-    selection_fg = "#b4bdc3",
+    foreground    = "#BBBBBB",
+    background    = "#191919",
+    cursor_fg     = "#191919",
+    cursor_bg     = "#C9C9C9",
+    cursor_border = "#191919",
+    selection_fg  = "#BBBBBB",
+    selection_bg  = "#404040",
 
     ansi = {
-      "#221f1d",
-      "#de6e7c",
-      "#819b69",
-      "#b77e64",
-      "#6099c0",
-      "#b279a7",
-      "#66a5ad",
-      "#b4bdc3",
+      "#191919",
+      "#DE6E7C",
+      "#819B69",
+      "#B77E64",
+      "#6099C0",
+      "#B279A7",
+      "#66A5AD",
+      "#BBBBBB"
     },
 
     brights = {
-      "#403833",
-      "#e8838f",
-      "#8bae68",
-      "#d68c67",
-      "#61abda",
-      "#cf86c1",
-      "#65b8c1",
-      "#888f94",
-    }
+      "#3D3839",
+      "#E8838F",
+      "#8BAE68",
+      "#D68C67",
+      "#61ABDA",
+      "#CF86C1",
+      "#65B8C1",
+      "#8E8E8E"
+    },
   },
 }
